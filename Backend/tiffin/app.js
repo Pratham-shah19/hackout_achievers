3e8e3cf677994ffe49445c8767cceac925b60311
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2022-09-24 02:38:00 +0530

    backend-tiffin-api-startup

---
+++ Backend/tiffin/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express();
+
+//dependencies
+require('express-async-errors')
+require('dotenv').config()
+const authMiddleware = require('./middleware/authorization')
+
+//router
+const mainRouter = require('./routes/router')
+
+//model
+const tiffinModel = require('./models/tiffin')
+
+//connect db
+const connectDb = require('./db/connect')
+
+//middlewares
+app.use(express.static('public'))
+app.use(express.json())
+
+//router
+app.use('/api/v1/tiffin',authMiddleware,mainRouter)
+
+const port = process.env.PORT || 3000;
+
+const start = async()=>{
+    try{
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(`Server listening on port ${port}`)
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+start();
+
---
+++ Backend/tiffin/models/order.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+//schema definition
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: [true, "Please provide user id"],
+    unique: true,
+  },
+  basketId: {
+    type: String,
+    required: [true, "Please provide basket id"],
+    unique: true,
+  },
+  deliveryId: {
+    type: String,
+    required: [true, "Please provide delivery id"],
+    unique: true,
+  },
+  tiffinId: {
+    type: String,
+    required: [true, "Please provide tiffin id"],
+    unique: true,
+  },
+  price: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    required: false,
+    default: "NEW",
+    enum: [
+      "NEW",
+      "COOKING",
+      "ACCEPTED",
+      "READY_TO_PICK",
+      "PICKED_UP",
+      "COMPLETED",
+    ],
+  },
+  deliveryFee:{
+    type:Number,
+    required:[true,'Please provide delivery fee']
+  }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
